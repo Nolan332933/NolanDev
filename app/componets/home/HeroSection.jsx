9d51237c8bbc2b5f0aa9d2609669008fa742c0de
Nolan332933 <nolansehlby81@gmail.com>
2024-11-12 15:37:05 +0630

    fix hero abit

--- app/componets/home/HeroSection.jsx
+++ app/componets/home/HeroSection.jsx
@@ -1,22 +1,16 @@
-import React from 'react'
-import Image from 'next/image'
-import HeroImage from '../../../public/image/dog2.png'
-import { Typewriter} from 'react-simple-typewriter'
-import Button from '../../../app/utlis/Button'
-
+import React from 'react';
+import { Typewriter } from 'react-simple-typewriter';
+import Button from '../../../app/utlis/Button';
 
 const HeroSection = () => {
   return (
-    <div className='relative bg-slate-100 text-black dark:bg-gray-800 dark:text-white'>
-      
-      <Image
-        src={HeroImage}
-        className='min-h-screen w-full object-cover object-center z-0' 
-        priority
-      />
-      <div className='text-center absolute top-1/2 text-white w-full md:w-2/3 translate-y-[-50%]'>
-        <div className='text-3xl text-orange-900 dark:text-white'> Hello, my soon-to-be coworkers! <span> I am Kaung Myat Kywal(Nolan)</span></div>
-        <h1 style={{fontSize : '60px'}} className='font-bold mt-10 text-orange-900 dark:text-white'>I am 
+    <div className='relative min-h-screen bg-slate-100 text-black dark:bg-gray-800 dark:text-white flex items-center justify-center'>
+      <div className='flex flex-col items-center text-center w-full md:w-2/3'>
+        <div className='text-4xl text-yellow dark:text-white'>
+          Hello, my soon-to-be coworkers! <span> I am Kaung Myat Kywal(Nolan)</span>
+        </div>
+        <h1 style={{ fontSize: '60px' }} className='font-bold mt-10 text-orange-900 dark:text-white'>
+          I am 
           <span className='ms-3 text-violet-700'> 
             <Typewriter  
               words={['Full Stack', 'Laravel', 'NodeJs', 'React', 'TypeScript', 'Python', 'Ai']}
@@ -33,7 +27,7 @@ const HeroSection = () => {
         <Button link="/contact" name="Hire Me" />
       </div>
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
